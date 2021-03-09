7b854a25473bfd11fd996da804670127fd1d256c
Csabi <szabo.csaba87@live.com>
2021-03-09 16:40:26 +0100

    create reddit-app and insatll firebase and bootstrap

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5aZk25KQkjQBMTGZzjCqstchKR1Y071I",
+    authDomain: "reddit-app-9dc12.firebaseapp.com",
+    projectId: "reddit-app-9dc12",
+    storageBucket: "reddit-app-9dc12.appspot.com",
+    messagingSenderId: "473455154080",
+    appId: "1:473455154080:web:c4441d9c12c8b6cf198ac5"
+};
+
+export const auth = app.auth();
+export default app;
