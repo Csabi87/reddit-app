3218acee8a2d458bb5fc40f925e15b0cd1bc8ac8
Csabi <szabo.csaba87@live.com>
2021-03-11 19:02:47 +0100

    basic style was implemented, update packages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import './App.scss';
-import db from './firebase.config.js';
-import React,{useState,useEffect} from 'react';
+import "./App.css";
+import db from "./firebase.config.js";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [posts, setPosts] = useState([]);
 
-  useEffect(()=> {
-    db.collection('posts').get()
+  useEffect(() => {
+    db.collection("posts")
+      .get()
       .then((querySnapshot) => {
         const data = [];
         querySnapshot.forEach((doc) => {
           const document = {
-              ...doc.data(),
-              id: doc.id
+            ...doc.data(),
+            id: doc.id,
           };
           data.push(document);
-        })
+        });
         setPosts(data);
       });
-
   }, []);
-
 
   return (
     <main>
-      {posts.map(post => (
+      <header className="header">
+        <h1 className="siteName">r/SPACE</h1>
+      </header>
+      {posts.map((post) => (
         <div className="container" key={post.id}>
           <div className="vote">
-            <p>{post.score}</p>
+            <p>fel</p>
+            <p>{post.vote}</p>
+            <p>le</p>
           </div>
           <div className="content">
             <h1>{post.title}</h1>
-            {post.content &&
-            <p>{post.content}</p>
-          }
-            <img src={post.url} alt=""/>
+            {post.content && <p>{post.content}</p>}
+            <div className="img-container">
+              <img src={post.url} alt="" />
+            </div>
           </div>
         </div>
       ))}
